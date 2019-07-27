24f04104d39390e6b716792908133b868469c51b
Gurwinder <gurwinderiam@gmail.com>
2019-07-28 02:34:39 +0530

    Update coin-values.scm

--- scheme/ch2/coin-values.scm
+++ scheme/ch2/coin-values.scm
@@ -7,10 +7,6 @@
 
 
 ;returing nth value inside list
-(define (list-ref items n)
-  (if (eq? n 0)
-      (car items)
-      (list-ref (cdr items) (- n 1))))
 (define squares (list 1 4 9 16 25))
 
 (define (length items)
@@ -34,6 +30,10 @@
 (define us-coins (list 50 25 10 5 1))
 (define uk-coins (list 100 50 20 10 5 2 1 0.5))
 
+(define (list-ref items n)
+  (if (= n 0)
+      (if (null? items) 'error (car items))
+      (list-ref (cdr items) (- n 1))))
 (define (cc amount coin-values)
   (cond ((= amount 0) 1)
         ((or (< amount 0) (no-more? coin-values)) 0)
@@ -46,7 +46,11 @@
                     coin-values))
                 coin-values)))))
 (define (first-denomination kind-of-coins)
-  (list-ref (us-coins) kind-of-coins))
+  (list-ref kind-of-coins 0))
+(define (except-first-denomination x) (cdr x))
+(define (no-more? xs) (null? xs))
+
+
 (define (cc-v1 amount kinds-of-coins)
   (cond ((= amount 0) 1)
         ((or (< amount 0) (= kinds-of-coins 0)) 0)
@@ -56,12 +60,10 @@
                         (first-denomination-v1
                          kinds-of-coins))
                      kinds-of-coins)))))
-(define (except-first-denomination x) (- x 1))
 (define (first-denomination-v1 kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
         ((= kinds-of-coins 2) 5)
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
-(define (no-more? xs) (null? xs))
-
+(cc 100 us-coins)
