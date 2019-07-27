87be49d52c4fbfd222241e3b65525c7490509b1e
Gurwinder <gurwinderiam@gmail.com>
2019-07-27 18:57:26 +0530

    Update conscadr.scm

--- scheme/ch2/conscadr.scm
+++ scheme/ch2/conscadr.scm
@@ -8,3 +8,10 @@
   dispatch)
 (define (car z) (z 0))
 (define (cdr z) (z 1))
+
+(define (cons-v1 x y)
+  (lambda (m) (m x y)))
+(define (car-v1 z)
+  (z (lambda (p q) p)))
+(define (cdr-v1 z)
+  (z (lambda (p q) q)))
