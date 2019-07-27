b4cecf08c71efa2127c1981146f426f2699b365a
Gurwinder <gurwinderiam@gmail.com>
2019-07-27 20:54:24 +0530

    Update interval-arithmetic.scm

--- scheme/ch2/interval-arithmetic.scm
+++ scheme/ch2/interval-arithmetic.scm
@@ -16,5 +16,29 @@
    (make-interval (/ 1.0 (upper-bound y))
                   (/ 1.0 (lower-bound y)))))
 (define (make-interval a b) (cons a b))
+
+;excercise 2.7
+;define upper-bound and lower-bound
 (define (lower-bound xs) (car xs))
 (define (upper-bound xs) (cdr xs))
+
+;excercise 2.8
+;using reasoning analogous to alyssa's
+;determine how the difference of two intervals may be computer?
+;define a corresponding subsctraction procedure, sub-interval
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (lower-bound y))
+                 (- (upper-bound x) (upper-bound y))))
+
+;excercise 2.9
+;the width of interval is half of the difference bw its upper and lower bounds
+;the width is uncertainty of the number specified by the interval
+;for some arithmetic operations the width of the result of combining
+;two intervals is a function only of the widths of the argument intervals,
+;whereas for others the width of combination is not a function of the widths
+;of argument intervals
+;1: Show that the width of the sum(or difference) of two intervals is a function
+;only of the intervals being added or substracted.
+;2: Give examples to show that this is not true for multiplication or division.
+
+
