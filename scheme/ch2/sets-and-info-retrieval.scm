0ec93c33fbece4a369b6304cadf08be24415d7fa
Gurwinder <gurwinderiam@gmail.com>
2019-07-29 02:07:19 +0530

    Create sets-and-info-retrieval.scm

---
+++ scheme/ch2/sets-and-info-retrieval.scm
@@ -0,0 +1,9 @@
+;SETS AND INFORMATION RETRIEVAL
+
+(define (key x) x)
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+        ((equal? given-key (key (car set-of-records)))
+         (car set-of-records))
+        (else (lookup given-key (cdr set-of-records)))))  
