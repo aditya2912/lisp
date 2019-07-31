44f73da15de771cba130079c1cf75586cc231b20
Gurwinder <gurwinderiam@gmail.com>
2019-08-01 01:26:36 +0530

    Create memorise-me.scm

---
+++ hackerearth/arrays/1D/memorise-me.scm
@@ -0,0 +1,31 @@
+#lang racket
+
+
+(define (read-ls l)
+  (cond
+    ((eq? l 0) '())
+    (else (cons (read) (read-ls (- l 1))))))
+
+(define (read-in l)
+  (cond
+    ((eq? l 0) '())
+    (else (cons (read) (read-in (- l 1))))))
+
+(define (N) (read-in (read)))
+(define (Q) (read-ls (read)))
+(define (empty-ls? xs) (eq? xs '()))
+
+(define (existence xs x)
+  (cond
+    ((and (not (empty-ls? xs)) (eq? (car xs) x)) (+ 1 (existence (cdr xs) x)))
+    ((and (not (empty-ls? xs))) (existence (cdr xs) x))
+    (else 0)))
+
+(define (existence? xs x)
+  (define a (existence xs x))
+  (if (not (= a 0)) a "NOT PRESENT"))
+
+(let
+    ((q (Q))
+     (n (N)))
+  (map (lambda (x) (let () (display (existence? q x)) (newline)) ) n))
